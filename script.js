6ddfc3530953572d698b066a8a5b85d67b7da3b9
Rodolpho <nikersonrodolpho@gmail.com>
2022-07-30 18:26:01 -0300

    Primeira parte

---
+++ script.js
@@ -0,0 +1,16 @@
+const dish1 = 'Caesar Salad'
+const kCal1 = 200
+const vegan1 = false
+
+const dish2 = 'The Hamburger'
+const kCal2 = 600
+const vegan2 = false
+
+const dish3 = 'Cauliflower Hot Wings'
+const kCal3 = 550
+const vegan3 = true
+
+const averageCalories = (kCal1 + kCal2 + kCal3) / 3
+console.log(averageCalories)
+
+console.log(vegan1 && vegan2 && vegan3)
