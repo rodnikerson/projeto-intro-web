1f926e5e3b8a120594480119aff5220462713234
Rodolpho <nikersonrodolpho@gmail.com>
2022-09-04 22:32:03 -0300

    Parte 5 e 6 do projeto

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ const vegan3 = true
 const ingredients3 = ['Cauliflower', 'Salt', 'Pepper', 'Vegan Hot Sauce', 'Coconut Oil', 'Garlic Powder', 'Paprika']
 
 const averageCalories = (kCal1 + kCal2 + kCal3) / 3
-console.log(averageCalories)
+// console.log(averageCalories)
 
-console.log(vegan1 && vegan2 && vegan3)
-console.log(dish1.toUpperCase(), dish2.toUpperCase(), dish3.toUpperCase())
+// console.log(vegan1 && vegan2 && vegan3)
+// console.log(dish1.toUpperCase(), dish2.toUpperCase(), dish3.toUpperCase())
 
 dishInfos0 = {
     dish: 'Caesar Salad',
@@ -68,7 +68,7 @@ menuStorage.forEach((elem) => elem.vegan ? menu.push(elem) : console.log(`Não f
 let ingredientsStorage = []
 
 for (i = 0; i < menu.length; i++) {
-    console.log(menu[i].ingredients)
+    // console.log(menu[i].ingredients)
     for (j = 0; j < menu[i].ingredients.length; j++) {
         ingredientsStorage.push(menu[i].ingredients[j])
     }
@@ -76,7 +76,33 @@ for (i = 0; i < menu.length; i++) {
 
 let ingredientsStorageString = ingredientsStorage.toString()
 
-console.log(ingredientsStorageString)
+// console.log(ingredientsStorageString)
 
 // Importante salientar que o loop com 'i' e 'j' acima foram feitos porque foram requisitados no exercício
 // afinal, seria muito mais simples usar o método .toString() direto - mas os loops reforçam a lógica
+
+// function stringOfObject(obj) {
+//     let objStorage = []
+//     for(i = 0; i < obj.length; i++) {
+//         for(elem in obj[i]) {
+//             objStorage.push((obj[i][elem]))
+//         }
+//     }
+//     console.log(objStorage)
+// }
+
+// stringOfObject(menuStorage)
+
+let string = 'vegan'
+
+function returnEqual(obj, string) {
+    let objReturn = ''
+    for(i = 0; i < obj.length; i++) {
+        for(elem in obj[i]) {
+            elem === string ? objReturn += (string + ': ' + obj[i][elem] + ' ') : console.log('Nenhum item encontrado')
+        }
+    }
+    console.log(objReturn)
+}
+
+returnEqual(menuStorage, string)
